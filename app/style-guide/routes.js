a2bf28e8499e7813ad85a715c7b1106c9ad68480
JFloMixpo <jflorence@mixpo.com>
2015-05-15 14:44:23 -0700

    TH-143 single select plus merge stuff from Alex

--- app/style-guide/routes.js
+++ app/style-guide/routes.js
@@ -17,6 +17,7 @@ define(function (require) {
     require('tpl!./dropdown.html');
     require('tpl!./checkboxes.html');
     require('tpl!./radio-buttons.html');
+    require('tpl!./select.html');
     require('tpl!./multi-select.html');
     require('tpl!./tooltip.html');
     require('tpl!./nav.html');
