ec7379e9dfc6e1542794f2f1616578f7affc4d9c
Paul Cassidy <pcassidy@mixpo.com>
2015-05-27 14:50:16 -0700

    Adding tests for tooltip directive. Refactoring tooltip.js for easier testability.

---
+++ app/core/directives/tooltip_test.js
@@ -0,0 +1,97 @@
+/**
+ * Created by paul on 5/21/15.
+ */
+define(function (require) {
+    'use strict';
+
+    require('./tooltip');
+    require('angularMocks');
+
+    describe('tooltipDirective', function () {
+        var compile,
+            rootScope,
+            document;
+
+
+        beforeEach(function () {
+            module('app.core');
+        });
+
+        // Store references to $rootScope and $compile
+        // so they are available to all tests in this describe block
+        beforeEach(inject(function ($compile, $rootScope, $document) {
+            // The injector unwraps the underscores (_) from around the parameter names when matching
+            compile = $compile;
+            rootScope = $rootScope;
+            document = $document;
+        }));
+
+        function testDimsAndClass(dims, className) {
+            var scope = rootScope.$new();
+            compile('<div tooltip="test"></div>')(scope);
+            scope.$digest();
+            expect(scope.calculateClass(dims)).toBe(className);
+        }
+
+        it('should be tooltip-top-center', function () {
+            var dims = {
+                right: 250,
+                left: 250,
+                top: 60
+            };
+
+            testDimsAndClass(dims, 'tooltip-top-center');
+        });
+
+        it('should be tooltip-top-left', function () {
+            var dims = {
+                right: 50,
+                left: 250,
+                top: 60
+            };
+
+            testDimsAndClass(dims, 'tooltip-top-left');
+        });
+
+        it('should be tooltip-top-right', function () {
+            var dims = {
+                right: 50,
+                left: 0,
+                top: 60
+            };
+
+            testDimsAndClass(dims, 'tooltip-top-right');
+        });
+
+        it('should be tooltip-bottom-center', function () {
+            var dims = {
+                right: 250,
+                left: 250,
+                top: 0
+            };
+
+            testDimsAndClass(dims, 'tooltip-bottom-center');
+        });
+
+        it('should be tooltip-bottom-left', function () {
+            var dims = {
+                right: 50,
+                left: 250,
+                top: 0
+            };
+
+            testDimsAndClass(dims, 'tooltip-bottom-left');
+        });
+
+        it('should be tooltip-bottom-right', function () {
+            var dims = {
+                right: 50,
+                left: 0,
+                top: 0
+            };
+
+            testDimsAndClass(dims, 'tooltip-bottom-right');
+        });
+    });
+
+});
