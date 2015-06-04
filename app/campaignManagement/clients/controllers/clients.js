6d1bbfed09f0263d2eb0c583b56b594112458f9d
Alexander Juarez <couatli@gmail.com>
2015-06-04 16:35:12 -0700

    updated the clients sortby

--- app/campaignManagement/clients/controllers/clients.js
+++ app/campaignManagement/clients/controllers/clients.js
@@ -4,7 +4,7 @@ define(function (require) {
     var app = require('./../../module');
 
     app.controller('clientsCtrl', ['$scope', '$http', '$timeout', 'topClientsService', function ($scope, $http, $timeout, topClients) {
-        topClients.init('/api/v3/clients?dimensions=id,name,channel,lastViewedUserDate,lastViewedUserName&metrics=impressions,countAccountsActive,countCampaignsPreFlight,countCampaignsInFlight&order=metrics.impressions&limit=10');
+        topClients.init('/api/v3/clients?dimensions=id,name,channel,lastViewedUserDate,lastViewedUserName&metrics=impressions,countAccountsActive,countCampaignsPreFlight,countCampaignsInFlight&sortBy=metrics.impressions&limit=10');
 
         topClients.observe(updateTopClients, $scope);
 
