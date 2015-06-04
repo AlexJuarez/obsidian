98c7e0ce6d3af005bd2a21475a60c1a257ad633d
Max Winderbaum <maxwinderbaum@gmail.com>
2015-06-04 10:05:55 -0700

    Added force-fixture to mock server, adjusted campaignsByStatus for new DB

--- server/mocks/thorwhalData/campaigns.js
+++ server/mocks/thorwhalData/campaigns.js
@@ -2,10 +2,17 @@ var endpointFactory = require( "../../utils/endpointFactory" );
 var collectionLogic = require( "../../utils/collectionLogic" );
 var getCollectionProperties = require( "../../utils/getCollectionProperties" );
 var fixture = require( "../../fixtures/thorwhalData/campaigns" );
+var campaignsByStatus = require( "../../fixtures/thorwhalData/custom/campaignsByStatus.json" );
 
 var endpoint = endpointFactory.createEndpoint( {
 	fixture: fixture.campaigns,
 	endpointName: "campaigns",
+    fixtureEndpoints: [
+        {
+            queryParams: "?dimensions=id,name,startDate,endDate,budget,bookedImpressions&metrics=countPlacements,countCreatives,impressions&filters=status:eq:",
+            fixture: campaignsByStatus
+        }
+    ],
 	responseLogic: function( req, res, items ) {
 		var campaigns = collectionLogic( req, items );
 		campaigns = getCollectionProperties( {
--- server/utils/endpointFactory.js
+++ server/utils/endpointFactory.js
@@ -1,24 +1,37 @@
-var expandParents = require( "./expandParents" );
+var expandParents = require("./expandParents");
 
 module.exports = {
-	/**
-	 *
-	 * @param data {
+    /**
+     *
+     * @param data {
 	 * 		endpointName: string,
 	 * 		fixture: string*,
 	 * 		responseLogic: function( req, res, items ) - what to send back
 	 *
 	 * }
-	 * @returns {Function}
-	 */
-	createEndpoint: function( data ) {
+     * @returns {Function}
+     */
+    createEndpoint: function (data) {
 
-		expandParents( data.fixture );
+        expandParents(data.fixture);
 
-		return function( app ) {
-            app.get( "/api/v3/" + data.endpointName, function( req, res ) {
-				return data.responseLogic( req, res, data.fixture );
-			} );
+        return function (app) {
+            app.get("/api/v3/" + data.endpointName, function (req, res) {
+                var queryParams = req._parsedUrl.search;
+                var matchedQueryParams = false;
+                if (data.fixtureEndpoints) {
+                    data.fixtureEndpoints.forEach(function (fixtureEndpoint) {
+                        if (queryParams.indexOf(fixtureEndpoint.queryParams) === 0) {
+                            res.send(fixtureEndpoint.fixture);
+                            matchedQueryParams = true;
+                        }
+                    });
+                }
+
+                if (!matchedQueryParams) {
+                    return data.responseLogic(req, res, data.fixture);
+                }
+            });
         };
-	}
+    }
 };
