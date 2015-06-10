cecf0513cca9df7c7122dcab1b6534e4477335ca
Max Winderbaum <maxwinderbaum@gmail.com>
2015-06-09 20:33:31 -0700

    added support for mixpo_server.identity

--- app/core/factories/domainInterceptor.js
+++ app/core/factories/domainInterceptor.js
@@ -3,8 +3,17 @@ define(function (require) {
 
     var module = require('./../module');
 
+    var apiPrefixes = ['/api/v3', '/api/v2', '/narwhal'];
+
     module.factory('domainInterceptor', [function () {
         function request(config) {
+            if(window.apiURI) {
+                apiPrefixes.forEach(function(apiPrefix) {
+                    if (config.url.indexOf(apiPrefix) > -1) {
+                        config.url = window.apiURI + config.url;
+                    }
+                });
+            }
             return config;
         }
 
