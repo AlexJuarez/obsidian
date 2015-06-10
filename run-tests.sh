830bd2c3f7b81bfa8bd98d7ec6d8a6d7eee4bd74
Alexander Juarez <couatli@gmail.com>
2015-06-10 12:22:12 -0700

    added a tests script

--- run-tests.sh
+++ run-tests.sh
@@ -1,1 +1,29 @@
-node ./node_modules/karma/bin/karma --single-run
+npm install
+node ./node_modules/bower/bin/bower install
+
+set +e
+set +x
+
+results=$(node ./node_modules/karma/bin/karma start --single-run)
+
+fail=$(echo $results | tail -1 | grep FAIL)
+
+if [ -z "${fail// }" ]
+then
+summary=$(cat coverage/text-summary.txt)
+coverage=$(echo $summary | grep -o 'Statements[ ]*:[ ]*[0-9]*.[0-9]*%' | awk -F" " '{print $3}')
+info=$(echo "$results" | tail -1 | awk -F":" '{print $2}')
+curl -XPOST -H "Authorization: token 40da8eeed353283720cc14c7433beb5462082c53" https://api.github.com/repos/Mixpo/obsidian/statuses/$(git rev-parse HEAD) -d "{
+  \"state\": \"success\",
+  \"target_url\": \"${BUILD_URL}\",
+  \"description\": \"$info. ${coverage} Statement Coverage.\"
+}"
+else
+curl -XPOST -H "Authorization: token 40da8eeed353283720cc14c7433beb5462082c53" https://api.github.com/repos/Mixpo/obsidian/statuses/$(git rev-parse HEAD) -d "{
+  \"state\": \"failure\",
+  \"target_url\": \"${BUILD_URL}\",
+  \"description\": \"Build Failing. ${coverage} Statement Coverage.\"
+}"
+set -e
+exit 1
+fi
