a241aa4d4054aa6e67bf56f4265a5465296ce8b1
Paul Cassidy <pcassidy@mixpo.com>
2015-06-16 15:51:07 -0700

    Adding test for navbar directives.

---
+++ app/core/navbar/directives/campaignDropdown_test.js
@@ -0,0 +1,140 @@
+define(function (require) {
+    'use strict';
+
+    require('./campaignDropdown');
+    require('angularMocks');
+
+    var template = require('tpl!./campaign.html');
+
+    describe('campaignDropdownDirective', function () {
+        var compile, rootScope, document, campaign, navbar, account;
+
+        beforeEach(function () {
+            module('app.core');
+        });
+
+        beforeEach(inject(function ($compile, $rootScope, $document, $templateCache, navbarService, campaignService, accountService) {
+            $templateCache.put('core/navbar/directives/campaign.html', template);
+
+            compile = $compile;
+            rootScope = $rootScope;
+            document = $document;
+            navbar = navbarService;
+            campaign = campaignService;
+            account = accountService;
+        }));
+
+        function createDropDown(campaigns) {
+            campaign.setData(campaigns);
+            account.setData([{
+                'id': 'accountId0',
+                'name': 'Account 0',
+                'pinned': false,
+                'active': false,
+                'lastViewed': '2015-01-01T12:00:00Z',
+                'lastViewedName': 'Joe Snoopypants',
+                'client': {'id': 'clientId0'},
+                'division': {'id': 'divisionId0'}
+            },
+                {
+                    'id': 'accountId1',
+                    'name': 'Account 1',
+                    'pinned': false,
+                    'active': false,
+                    'lastViewed': '2015-01-01T12:00:00Z',
+                    'lastViewedName': 'Jo Ma Ma',
+                    'client': {'id': 'clientId1'},
+                    'division': {'id': 'divisionId1'}
+                }]);
+
+            var parentScope = rootScope.$new();
+            var html = compile('<div campaign-dropdown="test"></div>')(parentScope);
+            parentScope.$apply();
+
+            return html.scope();
+        }
+
+        describe('controller', function () {
+            var campaigns = [
+                {
+                    'id': 'campaignId0',
+                    'name': 'Campaign 0',
+                    'pinned': true,
+                    'status': 'preFlight',
+                    'goalImpressions': 1000000,
+                    'lastViewed': '2015-01-01T12:00:00Z',
+                    'lastViewedName': 'Joe Snoopypants',
+                    'startDate': '2015-02-01',
+                    'endDate': '2015-03-01',
+                    'client': {id: 'clientId0'},
+                    'division': {id: 'divisionId0'},
+                    'account': {id: 'accountId0'},
+                    'metrics': {
+                        'currentImpressions': 15000,
+                        'placementCount': 10,
+                        'creativeCount': 10
+                    }
+                }
+            ];
+
+            it('should check to see if elements are pinned.',function(){
+                var scope = createDropDown(campaigns);
+
+                expect(scope.pinned).toEqual([campaign.all()[0]]);
+            });
+
+            it('should check to see if the quarter map exists',function(){
+                var scope = createDropDown(campaigns);
+
+                expect(scope.quarterMap).toEqual(campaign.quarterMap());
+            });
+
+            it('should change the pinned campaign when pin state change', function() {
+                var scope = createDropDown(campaigns);
+
+                scope.unpin(campaign.all()[0]);
+                expect(scope.pinned.length).toEqual(0);
+
+                scope.pin(campaign.all()[0]);
+                expect(scope.pinned.length).toEqual(1);
+            });
+
+            it('should have the current campaign', function() {
+                var scope = createDropDown(campaigns);
+
+                expect(scope.current).toEqual('All Campaigns');
+            });
+
+            it('should update the query results on a search', function () {
+                var scope = createDropDown(campaigns);
+
+                scope.query = 'Campaign 0';
+                scope.$digest();
+
+                expect(scope.results).toEqual([campaign.all()[0]]);
+
+                scope.query = 'Campaign 01';
+                scope.$digest();
+
+                expect(scope.results).toEqual([]);
+            });
+
+            it('should return the current campaign', function () {
+                navbar.setData({campaignId: 'campaignId0'});
+
+                var scope = createDropDown(campaigns);
+
+                expect(scope.current).toEqual('Campaign 0');
+            });
+
+            it('should return the current campaign with account.', function () {
+                navbar.setData({campaignId: 'campaignId0', accountId: 'accountId0'});
+
+                var scope = createDropDown(campaigns);
+
+                expect(scope.current).toEqual('Campaign 0');
+            });
+
+        });
+    });
+});
---
+++ app/core/navbar/directives/divisionDropdown_test.js
@@ -0,0 +1,117 @@
+define(function (require) {
+    'use strict';
+
+    require('./divisionDropdown');
+    require('angularMocks');
+
+    var template = require('tpl!./division.html');
+
+    describe('divisionDropdownDirective', function () {
+        var compile, rootScope, document, division, navbar, client;
+
+        beforeEach(function () {
+            module('app.core');
+        });
+
+        beforeEach(inject(function ($compile, $rootScope, $document, $templateCache, navbarService, divisionService, clientService) {
+            $templateCache.put('core/navbar/directives/division.html', template);
+
+            compile = $compile;
+            rootScope = $rootScope;
+            document = $document;
+            navbar = navbarService;
+            division = divisionService;
+            client = clientService;
+        }));
+
+        function createDropDown(divisions) {
+            division.setData(divisions);
+            client.setData([{
+                'id': 'clientId0',
+                'name': 'Client 0',
+                'pinned': true,
+                'active': false,
+                'lastViewed': '2015-01-01T12:00:00Z',
+                'lastViewedName': 'Joe Snoopypants',
+                'channel': 'Advertisers'
+            }]);
+
+            var parentScope = rootScope.$new();
+            var html = compile('<div division-dropdown="test"></div>')(parentScope);
+            parentScope.$apply();
+
+            return html.scope();
+        }
+
+        describe('controller', function () {
+            var divisions = [
+                {
+                    'id': 'divisionId0',
+                    'name': 'Division 0',
+                    'pinned': true,
+                    'active': false,
+                    'lastViewed': '2015-01-01T12:00:00Z',
+                    'client': {'id': 'clientId0'}
+                },
+                {
+                    'id': 'divisionId1',
+                    'name': 'Division 1',
+                    'pinned': false,
+                    'active': false,
+                    'lastViewed': '2015-01-01T12:00:00Z',
+                    'client': {'id': 'clientId1'}
+                }
+            ];
+
+            it('should check to see if elements are pinned.',function(){
+                var scope = createDropDown(divisions);
+
+                expect(scope.pinned).toEqual([division.all()[0]]);
+            });
+
+            it('should check to see if the accountsMap exists',function(){
+                var scope = createDropDown(divisions);
+
+                expect(scope.divisionsMap).toEqual(division.alphabetMap());
+            });
+
+            it('should change the pinned account when pin state change', function() {
+                var scope = createDropDown(divisions);
+
+                scope.unpin(division.all()[0]);
+                expect(scope.pinned.length).toEqual(0);
+
+                scope.pin(division.all()[0]);
+                expect(scope.pinned.length).toEqual(1);
+            });
+
+            it('should have the current account', function() {
+                var scope = createDropDown(divisions);
+
+                expect(scope.current).toEqual('All Divisions');
+            });
+
+            it('should update the query results on a search', function () {
+                var scope = createDropDown(divisions);
+
+                scope.query = 'Division 0';
+                scope.$digest();
+
+                expect(scope.results).toEqual([division.all()[0]]);
+
+                scope.query = 'Division 01'; //real is 'Account 1'
+                scope.$digest();
+
+                expect(scope.results).toEqual([]);
+            });
+
+            it('should return the first division', function () {
+                navbar.setData({divisionId: 'divisionId0'});
+
+                var scope = createDropDown(divisions);
+
+                expect(scope.current).toEqual('Division 0');
+            });
+        });
+    });
+});
