b35fc7dd0dd35bc73595442183adb2654901a056
Max Winderbaum <maxwinderbaum@gmail.com>
2015-07-02 14:54:12 -0700

    started working on header

--- app/campaignManagement/campaigns/placements/controllers/list.js
+++ app/campaignManagement/campaigns/placements/controllers/list.js
@@ -4,6 +4,27 @@ define(function (require) {
     var app = require('./../../../module');
 
     app.controller('placementListCtrl', ['$scope', function ($scope) {
-        $scope.placements = 'placements go here';
+        $scope.placementTypes = [
+            {
+                name: 'Placement Type 1',
+                value: '1'
+            },
+            {
+                name: 'Placement Type 2',
+                value: '2'
+            },
+            {
+                name: 'Placement Type 3',
+                value: '3'
+            },
+            {
+                name: 'Placement Type 4',
+                value: '4'
+            },
+            {
+                name: 'Placement Type 5',
+                value: '5'
+            }
+        ];
     }]);
 });
