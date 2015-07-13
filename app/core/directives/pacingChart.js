d45213bbdae31312ee7cb97a03c6a1c53d1a1a5b
Max Winderbaum <maxwinderbaum@gmail.com>
2015-07-13 12:28:19 -0700

    fixed table fixtures for pacing chart's new naming schema

--- app/core/directives/pacingChart.js
+++ app/core/directives/pacingChart.js
@@ -13,14 +13,12 @@ define(function (require) {
                 var key = attr.pacingChart;
 
                 scope.$watch(key, function (data) {
-                    console.log(key, data);
                     if (data) {
                         scope.max = data.max;
                         scope.current = data.current;
                         scope.target = data.target;
 
                         var fill = d3.select(elem.find('.fill > rect')[0]);
-
                         fill.attr('width', Math.min(Math.round(data.current/data.max*100), 100) + '%');
                         //var target = d3.select(elem.find('.target > rect')[0]);
                         //target.attr('x', '80%');
