e80a75605271b6cc89154c60f93160a5c38872f3
Alexander Juarez <couatli@gmail.com>
2015-07-15 14:17:25 -0700

    adding back the controller

---
+++ app/campaignManagement/controllers/campaignManagement.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Alex on 3/2/2015.
+ */
+/* jshint camelcase: false */
+/* jshint -W098 */
+/* jshint -W004 */
+/* jshint -W101 */
+
+'use strict';
+
+define(function (require) {
+    var app = require('./../module');
+    //var ng = require('angular');
+
+    app.controller('campaignManagementCtrl', ['$scope', function ($scope) {
+    }]);
+});
