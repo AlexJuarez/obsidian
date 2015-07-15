88cb38e948ebf737f0c771657f17a364493c1641
Alexander Juarez <couatli@gmail.com>
2015-07-15 14:24:37 -0700

    added the new navbar states

--- app/core/navbar/navbar.js
+++ app/core/navbar/navbar.js
@@ -7,13 +7,26 @@ define(function (require) {
     var app = require('./../module');
     require('tpl!./navbar.html');
 
-    app.directive('navbar', [function () {
+    app.directive('navbar', ['$rootScope', '$state', function ($rootScope, $state) {
         return {
             restrict: 'A',
             templateUrl: 'core/navbar/navbar.html',
             replace: true,
             scope: {
                 open: '='
+            },
+            link: function (scope) {
+                scope.analytics = analytics;
+
+                function analytics(){
+                    if ($state.params.clientId) {
+                        $state.go('analytics.campaigns.client')
+                    }
+                }
+
+                $rootScope.$on('$stateChangeSuccess', function () {
+                    scope.open = false;
+                });
             }
         };
     }]);
