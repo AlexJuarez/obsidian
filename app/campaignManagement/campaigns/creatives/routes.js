aa229ab80e5d5ba09e1f1dfda45fa1e41aa67aee
Max Winderbaum <maxwinderbaum@gmail.com>
2015-07-21 14:45:57 -0700

    moved routes and templates from campaignMangement/routes.js into their appropriate sub-directories

---
+++ app/campaignManagement/campaigns/creatives/routes.js
@@ -0,0 +1,47 @@
+define(function (require) {
+    'use strict';
+    var app = require('./../../module');
+
+    require('tpl!./creativesList.html');
+    require('tpl!./creativesThumbnails.html');
+    require('tpl!./creativesHeader.html');
+    require('tpl!./directives/creativeThumbnails.html');
+
+    return app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.when('/campaign-management/campaign/:campaignId/creatives', '/campaign-management/campaign/:campaignId/creatives/list');
+
+        $stateProvider
+            .state({
+                name: 'cm.campaigns.detail.creatives',
+                url: '/creatives?filter'
+            })
+            .state({
+                name: 'cm.campaigns.detail.creatives.list',
+                url: '/list',
+                views: {
+                    'tab-header@cm.campaigns.detail': {
+                        controller: 'creativesHeaderCtrl',
+                        templateUrl: 'campaignManagement/campaigns/creatives/creativesHeader.html'
+                    },
+                    'table@cm.campaigns.detail': {
+                        controller: 'creativesListCtrl',
+                        templateUrl: 'campaignManagement/campaigns/creatives/creativesList.html'
+                    }
+                }
+            })
+            .state({
+                name: 'cm.campaigns.detail.creatives.thumbnails',
+                url: '/thumbnails',
+                views: {
+                    'tab-header@cm.campaigns.detail': {
+                        controller: 'creativesHeaderCtrl',
+                        templateUrl: 'campaignManagement/campaigns/creatives/creativesHeader.html'
+                    },
+                    'table@cm.campaigns.detail': {
+                        templateUrl: 'campaignManagement/campaigns/creatives/creativesThumbnails.html'
+                    }
+                }
+            });
+    }]);
+});
