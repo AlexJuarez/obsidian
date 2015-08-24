787fb8646e055c69cb9bf669f6eca2950b2f90f9
JFloMixpo <jflorence@mixpo.com>
2015-08-24 16:01:54 -0700

    TH-300 added css flex for other browsers

--- app/campaignManagement/campaigns/controllers/analyticsPreview.js
+++ app/campaignManagement/campaigns/controllers/analyticsPreview.js
@@ -18,17 +18,17 @@ define(function (require) {
         
         // Adjust bootstrap classes based on # of ad types returned
         if ($scope.creativeData.length <= 1) {
-            // set to small colClasses
+            // set to xs colClasses
             $scope.colClasses = 'col-xs-12';
         } else if ($scope.creativeData.length <= 2) {
             // set to med colClasses
             $scope.colClasses = 'col-xs-12 col-sm-6';
         } else if ($scope.creativeData.length <= 3) {
-            // set to med colClasses
-            $scope.colClasses = 'col-xs-12 col-sm-4';
-        } else {
             // set to large colClasses
-            $scope.colClasses = 'col-xs-12 col-sm-4 col-md-3';    
+            $scope.colClasses = 'col-xs-12 col-sm-6 col-md-4';
+        } else {
+            // set to x-large colClasses
+            $scope.colClasses = 'col-xs-12 col-sm-6 col-md-3';    
         }
 
 
