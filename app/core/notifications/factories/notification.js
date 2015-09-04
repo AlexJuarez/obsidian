8eaa563afd2c07a73d858af5b157aa996ec8f5b8
Alexander Juarez <couatli@gmail.com>
2015-09-04 11:46:37 -0700

    stubbed the notifications service, so far only console.logs the notification

---
+++ app/core/notifications/factories/notification.js
@@ -0,0 +1,30 @@
+define(function (require) {
+    'use strict';
+
+    var module = require('./../../module');
+
+    module.factory('notification', ['$rootScope', 'notificationService', function ($rootScope) {
+        function error(message, options) {
+            $rootScope.$broadcast('notifications:error', message, options);
+        }
+
+        function warn(message, options) {
+            $rootScope.$broadcast('notifications:warn', message, options);
+        }
+
+        function info(message, options) {
+            $rootScope.$broadcast('notifications:info', message, options);
+        }
+
+        function success(message, options) {
+            $rootScope.$broadcast('notifications:success', message, options);
+        }
+
+        return {
+            error: error,
+            warn: warn,
+            info: info,
+            success: success
+        }
+    }]);
+});
---
+++ app/core/notifications/services/notification.js
@@ -0,0 +1,18 @@
+define(function (require) {
+    'use strict';
+
+    var module = require('./../../module');
+
+    module.service('notificationService', ['$rootScope', function ($rootScope) {
+        var data = [];
+
+        $rootScope.$on('notifications:success', notificationHandler);
+        $rootScope.$on('notifications:warn', notificationHandler);
+        $rootScope.$on('notifications:error', notificationHandler);
+        $rootScope.$on('notifications:info', notificationHandler);
+
+        function notificationHandler(event, message, options) {
+            console.log(event, message, options);
+        }
+    }]);
+});
