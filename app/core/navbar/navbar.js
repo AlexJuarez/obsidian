0badb0f25a90d7bd4b503497337b5b977bc3b647
Alexander Juarez <couatli@gmail.com>
2015-09-04 14:00:32 -0700

    updated the build and linking between the sides

--- app/core/navbar/navbar.js
+++ app/core/navbar/navbar.js
@@ -29,7 +29,7 @@ define(function (require) {
                     } else if ($state.params.accountId) {
                         $state.go(area + '.campaigns.account', {accountId: $state.params.accountId});
                     } else if ($state.params.campaignId) {
-                        $state.go(area +'.campaigns.campaign', {campaignId: $state.params.campaignId});
+                        $state.go(area +'.campaigns.detail', {campaignId: $state.params.campaignId});
                     } else {
                         $state.go(area);
                     }
