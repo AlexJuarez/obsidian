418aff2799ed18fe6a39cbdbc916185edb981205
Max Winderbaum <maxwinderbaum@gmail.com>
2015-10-06 16:14:52 -0700

    updated assignPublisher to use new navbar refactor

--- app/campaignManagement/campaigns/placements/directives/assignPublisher.js
+++ app/campaignManagement/campaigns/placements/directives/assignPublisher.js
@@ -1,49 +1,40 @@
-define(function (require) {
+define(function(require) {
 	'use strict';
 
 	var app = require('./../../../module');
 
 	require('tpl!./assign-publisher.html');
 
-	app.directive('assignPublisher', [function () {
-		return {
-			restrict: 'A',
-			replace: true,
-			scope: {
-				publishers: '=',
-				placement: '='
-			},
-			templateUrl: 'campaignManagement/campaigns/placements/directives/assign-publisher.html',
-			controller: ['$scope', 'placementRecordService', 'campaignRecordService', 'accountRecordService',
-					'divisionRecordService', 'clientRecordService', 'clientPublisherRecordService',
-					function ($scope, placementRecordService, campaignRecordService,
-					accountRecordService, divisionRecordService, clientRecordService, clientPublisherRecordService
-			) {
-				$scope.$watch('placement', function() {
-					if ($scope.placement && $scope.placement.campaignId) {
-						updatePublishers($scope.placement.campaignId);
-					}
-				});
-
-				function updatePublishers(campaignId) {
-					campaignRecordService.fetch(campaignId)
-						.then(function(resp) {
-							accountRecordService.fetch(resp.data.accountId)
-								.then(function(resp) {
-									divisionRecordService.fetch(resp.data.divisionId)
-										.then(function(resp) {
-											clientRecordService.fetch(resp.data.clientId)
-												.then(function(resp) {
-													clientPublisherRecordService.fetch(resp.data.id)
-														.then(function(resp) {
-															$scope.publishers = resp.data;
-														});
-												});
-										});
-								});
+	app.directive('assignPublisher', [
+		function() {
+			return {
+				restrict: 'A',
+				replace: true,
+				scope: {
+					publishers: '=',
+					placement: '='
+				},
+				templateUrl: 'campaignManagement/campaigns/placements/directives/assign-publisher.html',
+				controller: [
+					'$scope', 'navbarService', 'clientPublisherRecordService',
+					function($scope, navbar, clientPublisherRecordService) {
+						updatePublishers();
+						navbar.observe(function() {
+							updatePublishers();
 						});
-				}
-			}]
-		};
-	}]);
+
+						function updatePublishers() {
+							var navbarData = navbar.all();
+							if(navbarData && navbarData.client && navbarData.client.id) {
+								clientPublisherRecordService.fetch(navbarData.client.id)
+									.then(function(resp) {
+										$scope.publishers = resp.data;
+									});
+							}
+						}
+					}
+				]
+			};
+		}
+	]);
 });
