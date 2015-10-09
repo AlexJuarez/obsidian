c8c4562bdb95944af7952d82c4625f58fd1b7225
Hays Clark <hclark@mixpo.com>
2015-10-09 00:19:14 -0700

    Moving studio Direct Adapter into studio folder
    Case: TH-541

--- app/campaignManagement/campaigns/creatives/index.js
+++ app/campaignManagement/campaigns/creatives/index.js
@@ -8,7 +8,7 @@ define(function (require) {
     require('./directives/creativeThumbnails');
     require('./directives/creativeOptions');
     require('./services/creatives');
-    require('./services/studioDirectAdapter');
+    require('./services/studio/studioDirectAdapter');
     require('./services/studio/urlBuilder/index');
     require('./services/newCreative');
     require('./services/creative');
