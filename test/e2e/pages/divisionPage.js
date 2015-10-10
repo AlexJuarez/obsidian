c6d5bb6554deecb31a2bb5dcf259cd19678573a3
Paul Cassidy <pcassidy@mixpo.com>
2015-10-09 17:58:12 -0700

    reorganizing.

---
+++ test/e2e/pages/divisionPage.js
@@ -0,0 +1,23 @@
+//Division page object
+var DivisionPage = (function(){
+
+// PAGE ELEMENTS
+// Selectors:
+// #  = id
+// .  = class
+// =  = contains specific text
+// *= = contains partial text match
+// xpath, css3 selectors also work.
+// 
+var newAccoutBtn = 'button = New Account',
+    editDivisionBtn = 'button = Edit Division',
+    campaignManagementTab = '#navbar-campaign-management-tab',
+    analyticsTab = '#navbar-analytics-tab',
+    viewByStatus = '',
+    viewByAccount = '',
+    searchAccountsField = '';
+
+//
+var 
+
+});
---
+++ test/e2e/spec/loginTest.spec.js
@@ -0,0 +1,13 @@
+define(function (require) {
+  require('../pages/loginPage');
+
+  describe('New Ad Build by LMP Creative Producer', function () {
+
+    it('will have no account ____', function(done) {
+      var loginPage = new LoginPage();
+      loginPage.loginToWebsite('rawr','lulz')
+        .call(done);
+    });
+
+  });  
+});
