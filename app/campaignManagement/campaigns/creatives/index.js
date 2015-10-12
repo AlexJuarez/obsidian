6968024359c932eb347fa302697d1a8dbb2c8418
Max Winderbaum <maxwinderbaum@gmail.com>
2015-10-12 10:45:39 -0700

    Removed require for missing file

--- app/campaignManagement/campaigns/creatives/index.js
+++ app/campaignManagement/campaigns/creatives/index.js
@@ -8,7 +8,6 @@ define(function (require) {
     require('./directives/creativeThumbnails');
     require('./directives/creativeOptions');
     require('./services/creatives');
-    require('./services/studio/studioCreativeAdapter');
     require('./services/studio/urlBuilder/index');
     require('./services/newCreative');
     require('./services/creative');
