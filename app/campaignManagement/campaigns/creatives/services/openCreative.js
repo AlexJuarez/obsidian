16638e10806426a4ad081fcb49dd0e47ceb7b4ed
Hays Clark <hclark@mixpo.com>
2015-10-14 22:56:03 -0700

    Prototyping using postMessage for cross window communication
    Case: TH-537

--- app/campaignManagement/campaigns/creatives/services/openCreative.js
+++ app/campaignManagement/campaigns/creatives/services/openCreative.js
@@ -15,22 +15,36 @@ define(function(require) {
      */
     module.service('openCreativeService', ['$q', '$window', 'studioUrlBuilder',
         function($q, $window, studioUrlBuilder) {
-            /**
-             * Opens studio and returns a promise.
-             *
-             * @param creative
-             * @returns {Object} promise
-             */
-            return function(creative, hostname) {
-                var deferred = $q.defer();
-                var url = studioUrlBuilder.open(creative.id, creative.campaignId)
+            function buildUrl(creative, hostname) {
+                return studioUrlBuilder.open(creative.id, creative.campaignId)
                     .setHostname(hostname)
                     .build();
-                var tabWindow = $window.open(
-                    url,
-                    'mixpo_studio'
-                );
+            }
 
+            function createPoller(tickMs, timeoutMs, onTick, onTimeout) {
+                var poll, timeout;
+
+                function start() {
+                    poll = setInterval(onTick, tickMs);
+                    timeout = setInterval(function() {
+                        stop();
+                        onTimeout();
+                    }, timeoutMs);
+                    return this;
+                }
+
+                function stop() {
+                    clearInterval(poll);
+                    clearInterval(timeout);
+                }
+
+                return {
+                    start: start,
+                    stop: stop
+                };
+            }
+
+            function createFallbackHandler(tabWindow, deferred) {
                 tabWindow.StudioDirectHandler = (function(){
                     function onClose(code, detail) {
                         if(code && detail) {
@@ -44,6 +58,49 @@ define(function(require) {
                         onClose: onClose
                     };
                 })();
+            }
+
+            /**
+             * Opens studio and returns a promise.
+             *
+             * @param creative
+             * @returns {Object} promise
+             */
+            return function(creative, hostname) {
+                var deferred = $q.defer();
+
+                var url = buildUrl(creative, hostname);
+                var tabWindow = $window.open(
+                    url,
+                    'mixpo_studio'
+                );
+
+
+                var poller = createPoller(500, 3000, function onTick() {
+                    tabWindow.postMessage('connect', '*');
+                }, function onTimeout() {
+                    createFallbackHandler(tabWindow, deferred);
+                }).start();
+
+                function receiveMessage(event) {
+                    //if (event.origin !== "http://example.org")
+                    //   return;
+
+                    if(event.data === 'connected') {
+                        return poller.stop();
+                    }
+
+                    console.log(event.data);
+                    // expect data as JSON string
+                    var cmd = JSON.parse(event.data);
+                    if(cmd.command === 'closeStudio') {
+                        tabWindow.close();
+                        deferred.resolve();
+                    } else {
+                        // unhandled event type
+                    }
+                }
+                window.addEventListener('message', receiveMessage, false);
 
                 return deferred.promise;
             };
